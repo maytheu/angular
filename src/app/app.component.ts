9fe95d1b17ad68a00c601f34f199dd25f0b5a7b0
maytheu <maytheu98@gmail.com>
2022-03-25 14:10:22 +0100

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typings';
+  randomText: string = lorem.sentence();
+  enteredText: string = '';
+  isSuccess: boolean = false;
+
+  onInput(e: any) {
+    this.enteredText = e.value;
+    if (this.randomText === this.enteredText) {
+      this.isSuccess = true;
+    }
+  }
+
+  compare(sentence: string, input: string) {
+    if (!input) {
+      return 'pending';
+    }
+    return sentence === input ? 'correct' : 'incorrect';
+  }
 }
