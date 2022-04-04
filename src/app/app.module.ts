46fb5ce0155b487bcf0f903bb57f2a89af3469ea
maytheu <maytheu98@gmail.com>
2022-04-04 19:49:44 +0100

    custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CustomClassDirective } from './custom-class.directive';
+import { TimesDirective } from './times.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomClassDirective
+    CustomClassDirective,
+    TimesDirective
   ],
   imports: [
     BrowserModule
--- src/app/custom-class.directive.ts
+++ src/app/custom-class.directive.ts
@@ -1,13 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appCustomClass]'
+  selector: '[appCustomClass]',
 })
 export class CustomClassDirective {
+  constructor(private element: ElementRef) {}
 
-  constructor() { 
-    console.log('custom directive');
-    
+  // @Input('appCustomClass') set backgroundColor(color: string) {
+  //   this.element.nativeElement.style.backgroundColor = color;
+  // }
+
+  // set classname
+  @Input('appCustomClass') set className(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
-
 }
