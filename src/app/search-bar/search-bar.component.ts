a25d8a11a76fe615190df034c8eaa8d9ceba9d5c
maytheu <maytheu98@gmail.com>
2022-04-21 09:32:14 +0100

    child paent communication

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
+  @Output() submitValue = new EventEmitter<string>();
+  term: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onInput(val: any) {
+    this.term = val.value;
   }
 
+  onFormSubmit(e: any) {
+    e.preventDefault();
+    this.submitValue.emit(this.term);
+  }
 }
