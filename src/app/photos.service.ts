a20c4f83fe472bcca8402603129f7ec6eaee8c8a
maytheu <maytheu98@gmail.com>
2022-05-13 09:55:30 +0100

    final commit

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,6 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface Photos{
+  urls:{
+    regular:string
+  }
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -8,7 +14,7 @@ export class PhotosService {
   constructor(private http: HttpClient) {}
 
   getPhoto() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
+    return this.http.get<Photos>('https://api.unsplash.com/photos/random', {
       headers: {
         Authorization:
           'Client-ID 08d19507529308e6b6874e4bce18a309019a26273da0556b967fa3969e97cf1',
--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotosService } from '../photos.service';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+  styleUrls: ['./photos.component.css'],
 })
 export class PhotosComponent implements OnInit {
+  photo=''
 
-  constructor() { }
+  constructor(private photoService: PhotosService) {}
 
   ngOnInit(): void {
+    this.reload()
   }
 
+  reload(){
+    this.photoService
+      .getPhoto()
+      .subscribe((data) => (this.photo = data.urls.regular));
+  }
 }
