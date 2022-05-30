0ba828a91581334294379c4c1ff8fd8ea6f33a7f
maytheu <maytheu98@gmail.com>
2022-05-30 10:15:25 +0100

    file geration and signup

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  signup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl(''),
+  });
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+}
